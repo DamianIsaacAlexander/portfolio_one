422432961d45cbc4598073cfffac9a031a6c8e58
Damian Isaac <damianisaacalexander@gmail.com>
2021-05-15 21:15:00 -0500

    fixed links

--- components/navigation.php
+++ components/navigation.php
@@ -4,8 +4,8 @@
     <a id="nav-icon" href="javascript:void(0);" onclick="myFunction()"><i class="fa fa-bars"></i></a>
     <ul id="nav-list">
         <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com">About</a></li>
-        <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com/projects.php">Projects</a></li>
-        <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com/resume.php">Resume</a></li>
-        <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com/contact.php">Contact</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com/pages/projects.php">Projects</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com/pages/resume.php">Resume</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://damianalexanderisaac.herokuapp.com/pages/contact.php">Contact</a></li>
     </ul>
 </nav>
