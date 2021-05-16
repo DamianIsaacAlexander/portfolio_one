b37cc1a8bea2d3951ec1450f52928f52bcf0f0cd
Damian Isaac <damianisaacalexander@gmail.com>
2021-05-15 21:09:10 -0500

    updated file struct

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function myFunction() 
-{
-    let x = document.getElementById("nav-list");
-    if(x.classList.contains("active"))
-    {
-        x.classList.remove("active");
-        x.classList.add("inactive");
-    } 
-    else
-    {
-        x.classList.add("active");
-        x.classList.remove("inactive")
-    }
+function myFunction() {
+  let x = document.getElementById("nav-list");
+  if (x.classList.contains("active")) {
+    x.classList.remove("active");
+    x.classList.add("inactive");
+  } else {
+    x.classList.add("active");
+    x.classList.remove("inactive");
+  }
 }
