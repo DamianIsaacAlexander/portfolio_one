8e8381ec2cc8a8a1d3ff0e6ce52b4b8d922d1112
Damian Isaac <damianisaacalexander@gmail.com>
2021-05-15 21:38:55 -0500

    fixed nav mobile

--- pages/contact.php
+++ pages/contact.php
@@ -10,7 +10,7 @@
     <script src="https://kit.fontawesome.com/7d91046a00.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,400;0,700;1,400&family=Merriweather:ital@0;1&display=swap" rel="stylesheet">
-    <script src="index.js" defer></script>
+    <script src="../index.js" defer></script>
 </head>
 <body>
     <?php include (dirname(__FILE__).'../../components/navigation.php')?>      
